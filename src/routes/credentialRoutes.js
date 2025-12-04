b46ce16e7eca7c8dbc21da993a6cc9749a7a6e98
alvinsanchez93 <159292482+alvinsanchez93@users.noreply.github.com>
2025-12-04 15:24:01 -0800

    Feat: Endpoint GET /credentials para listar y descifrar la bóveda

--- src/routes/credentialRoutes.js
+++ src/routes/credentialRoutes.js
@@ -4,10 +4,14 @@ const router = express.Router();
 const credentialController = require('../controllers/credentialController');
 const verifyToken = require('../middleware/authMiddleware');
 
-// PROTECCIÓN GLOBAL: Todas las rutas aquí requieren Token
+// --- PROTECCIÓN GLOBAL ---
+// Todas las rutas escritas aquí abajo requieren Token válido
 router.use(verifyToken);
 
-// POST /api/credentials -> Guardar nueva cuenta
+// 1. Guardar nueva credencial (POST /api/credentials)
 router.post('/', credentialController.createCredential);
+
+// 2. Obtener todas las credenciales (GET /api/credentials)
+router.get('/', credentialController.getAllCredentials);
 
 module.exports = router;
